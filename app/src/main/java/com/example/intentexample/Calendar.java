734a0fb3f9d3710cda75f69134c667bce793347a
kesler1202 <kesler@kaist.ac.kr>
2023-12-29 16:49:33 +0900

    material calendarview 적용

--- app/src/main/java/com/example/intentexample/Calendar.java
+++ app/src/main/java/com/example/intentexample/Calendar.java
@@ -1,74 +1,27 @@
 package com.example.intentexample;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
-import android.widget.CalendarView;
-import android.widget.EditText;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.text.InputType;
-import androidx.annotation.Nullable;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
 import androidx.annotation.NonNull;
-import android.util.Log;
-import java.util.HashSet;
-import java.util.Set;
-
+import androidx.fragment.app.Fragment;
 
 public class Calendar extends Fragment {
 
-    private static final String TAG = "CalendarFragment";
-    private CalendarView calendarView;
-    private EditText editTextPlan;
-    private Button saveButton;
-    private SharedPreferences sharedPreferences;
-    private Set<String> datesWithPlans;
-    private String selectedDate;
+    private MaterialCalendarView materialCalendarView;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.calendar, container, false);
 
-        calendarView = view.findViewById(R.id.calendarView);
-        editTextPlan = view.findViewById(R.id.editTextPlan);
-        saveButton = view.findViewById(R.id.saveButton);
+        materialCalendarView = view.findViewById(R.id.calendarView);
 
-        sharedPreferences = getActivity().getSharedPreferences("CalendarPlans", Context.MODE_PRIVATE);
-        datesWithPlans = new HashSet<>(sharedPreferences.getStringSet("datesWithPlans", new HashSet<>()));
-
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
-                editTextPlan.setText(sharedPreferences.getString(selectedDate, ""));
-            }
-        });
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "Save button clicked");
-                String plan = editTextPlan.getText().toString();
-                Log.d(TAG, "Saving plan for date " + selectedDate + ": " + plan);
-
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString(selectedDate, plan);
-                datesWithPlans.add(selectedDate);
-                editor.putStringSet("datesWithPlans", datesWithPlans);
-                editor.apply();
-
-                Log.d(TAG, "Plan saved");
-            }
-        });
+        // Select today's date by default
+        CalendarDay today = CalendarDay.today();
+        materialCalendarView.setSelectedDate(today);
 
         return view;
     }
