8b73d4f8e36f40ff329f7cab5e7ff1de6aec99c9
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-01 23:27:45 +0900

    [add] splash 화면 완성, gif 추가

---
+++ app/src/main/java/com/example/intentexample/Splash_Screen.java
@@ -0,0 +1,33 @@
+package com.example.intentexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // ActionBar 숨기기 전에 확인
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+    }
+}
